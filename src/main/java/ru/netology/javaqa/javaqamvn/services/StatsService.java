00e5b04c41fb7a55f48641580a6a11c686c5f79d
ElenaKr77 <elena.kriventsova77@gmail.com>
2025-05-10 18:15:51 +0300

    Change src

---
+++ src/main/java/ru/netology/javaqa/javaqamvn/services/StatsService.java
@@ -0,0 +1,71 @@
+package ru.netology.javaqa.javaqamvn.services;
+
+public class StatsService {
+    public int minSales(int[] sales) {
+        int minMonth = 0; // номер месяца с минимальными продажами среди просмотренных ранее
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[minMonth]) { // значит, в рассматриваемом i-м месяце продаж меньше
+                minMonth = i; // запомним его как минимальный
+            }
+        }
+        return minMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+    }
+
+    public int maxSales(int[] sales) {
+        int maxMonth = 0; // номер месяца с минимальными продажами среди просмотренных ранее
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[maxMonth]) { // значит, в рассматриваемом i-м месяце продаж меньше
+                maxMonth = i; // запомним его как минимальный
+            }
+        }
+        return maxMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+    }
+public int findAmountSales(int[] sales) {
+        int amountSales = 0;
+        for (int i = 0; i < sales.length; i++) {
+           amountSales+=sales[i];
+        }
+        return amountSales;
+}
+    public int averageAmountSalesPerMonth(int[] sales) {
+        int amountSales = 0;
+        for (int i = 0; i < sales.length; i++) {
+            amountSales+=sales[i];
+        }
+        amountSales = amountSales / sales.length;
+        return amountSales;
+    }
+    public int lowerAverage(int[] sales) {
+        int amountSales = 0;
+        for (int i = 0; i < sales.length; i++) {
+            amountSales+=sales[i];
+        }
+        amountSales = amountSales / sales.length;
+        int lowerMonth = 0;
+        for (int i = 0; i < sales.length; i++){
+            if (sales[i] < amountSales){
+                lowerMonth++;
+            }
+        }
+        return lowerMonth;
+
+    }
+    public int higherAverage(int[] sales) {
+        int amountSales = 0;
+        for (int i = 0; i < sales.length; i++) {
+            amountSales+=sales[i];
+        }
+        amountSales = amountSales / sales.length;
+        int higherMonth = 0;
+        for (int i = 0; i < sales.length; i++){
+            if (sales[i] > amountSales){
+                higherMonth++;
+            }
+        }
+        return higherMonth;
+
+    }
+
+
+
+}
